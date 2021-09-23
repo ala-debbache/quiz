1be7ac231ea5b0f45e70aa9f67ac52999a9a6a5e
ala-debbache <65853803+ala-debbache@users.noreply.github.com>
2021-09-23 23:00:14 +0100

    fixing cors problems

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const quizzesRouter = require('./routes/quizzes');
 const usersRouter = require('./routes/users');
 const completedRouter = require('./routes/completed');
@@ -10,6 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 app.use("/quizzes",quizzesRouter);
 app.use("/users",usersRouter);
 app.use("/completed",completedRouter);
@@ -20,4 +25,4 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(process.env.PORT || 3000,()=> console.log(`Server running`));
+app.listen(process.env.PORT || 3008,()=> console.log(`Server running on http://localhost:3008`));
